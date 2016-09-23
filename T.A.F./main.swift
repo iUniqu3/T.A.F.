1cd7e6825c1aa9bbaf070e2466814d3ee9f735f7
Arthur Berschader <iUniqu3@iuniqu3-mb-air.fritz.box>
2016-09-24 00:22:46 +0200

    Initial Commit

---
+++ T.A.F./main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  T.A.F.
+//
+//  Created by Arthur Berschader on 24.09.16.
+//  Copyright © 2016 Arthur Berschader. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
